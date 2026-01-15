50e1ba92ee50feec0dee392bea6f9206bf50be78
Bueno <marcos.bueno123456789@gmail.com>
2026-01-15 17:06:46 -0300

    fix: definir __dirname em ES modules

--- server/static.ts
+++ server/static.ts
@@ -1,3 +1,9 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
